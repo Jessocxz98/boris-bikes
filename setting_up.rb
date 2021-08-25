a5a174e3cd03122389eafec91f81aa8e2088f07c
Jessica Bulman <jessicabulman97@gmail.com>
2021-08-25 12:22:16 +0100

    setting up the program from user stories

---
+++ setting_up.rb
@@ -0,0 +1,9 @@
+# Nouns: Person, bike, docking station
+# Verbs: use bike, release bike, see if bike is working
+#
+# User               
+# Docking Station    release_bike
+# Bike               is_working?
+#
+# Bike <-- is_working? --> true/false
+# DockingStation <-- release_bike --> a Bike
